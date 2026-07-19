51a7df593024531aee2e0de6b4369d8cff65352d
Studio L4 <studiol4rj@gmail.com>
2026-07-19 18:25:51 -0300

    Desativa popup do showroom

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 const header = document.querySelector("[data-header]");
 const toggle = document.querySelector("[data-menu-toggle]");
 const navLinks = document.querySelectorAll("[data-nav] a");
-const showroomModal = document.querySelector("[data-showroom-modal]");
-const showroomClose = document.querySelector("[data-showroom-close]");
-const showroomSessionKey = "alumioro-showroom-seen";
-
-if (showroomModal && sessionStorage.getItem(showroomSessionKey) === "true") {
-  showroomModal.classList.add("is-hidden");
-}
 
 toggle?.addEventListener("click", () => {
   const isOpen = header.classList.toggle("is-open");
@@ -19,16 +12,4 @@ navLinks.forEach((link) => {
     header.classList.remove("is-open");
     toggle?.setAttribute("aria-expanded", "false");
   });
-});
-
-showroomClose?.addEventListener("click", () => {
-  showroomModal?.classList.add("is-hidden");
-  sessionStorage.setItem(showroomSessionKey, "true");
-});
-
-showroomModal?.addEventListener("click", (event) => {
-  if (event.target === showroomModal) {
-    showroomModal.classList.add("is-hidden");
-    sessionStorage.setItem(showroomSessionKey, "true");
-  }
 });
